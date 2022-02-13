abf5d239da55dc02940f7bbdd003f8ffc2e06335
Christopher Giroir <kelsin@valefor.com>
2022-02-12 22:15:13 -0800

    feat: idea logo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import logo from './logo.svg';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
         <iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/3Ne03199zyQ" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
       </header>
     </div>
